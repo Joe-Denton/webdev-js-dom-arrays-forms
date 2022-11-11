5ade1a524576b5cbf82e97af5e8dd00b0b9b4a5e
Joe-Denton <c2022171@hallam.shu.ac.uk>
2022-11-11 10:15:09 +0000

    Click event

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 (function(){
+//start
 document.getElementById("myHeading").innerHTML="Joe Denton"
+
 let firstListItem = document.querySelector("nav ul li")
 firstListItem.setAttribute("class", "currentPage")
+
 let firstLink = document.querySelector("nav ul li a")
 firstLink.setAttribute("href", "https://www.google.co.uk")
+//add a test event
+document.getElementById("myTestEvent").addEventListener("click", function(){
+    console.info("You have clicked me")
+})
+//end
 })()
-
