cc4fcf9090c7ae416ac57fff43f2bc6ae8597fb7
Joe-Denton <c2022171@hallam.shu.ac.uk>
2022-11-11 09:55:36 +0000

    Changed link

--- js/main.js
+++ js/main.js
@@ -2,5 +2,7 @@
 document.getElementById("myHeading").innerHTML="Joe Denton"
 let firstListItem = document.querySelector("nav ul li")
 firstListItem.setAttribute("class", "currentPage")
+let firstLink = document.querySelector("nav ul li a")
+firstLink.setAttribute("href", "https://www.google.co.uk")
 })()
 
