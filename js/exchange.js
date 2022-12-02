5c512849570c9867421258885a2034c8aa55048f
Joe-Denton <c2022171@hallam.shu.ac.uk>
2022-12-02 09:22:10 +0000

    yes

--- js/exchange.js
+++ js/exchange.js
@@ -1,17 +1,30 @@
 (function(){
 //start
 
-let pricePounds;
-let exchangeRate;
-let priceEuros;
-let convertForm = document.getElementById('myForm');
-let msg = document.getElementById('msg');
+let pricePounds
+let exchangeRate = 0.85
+let priceEuros = 0
+let convertForm = document.getElementById('myForm')
+let msg = document.getElementById('msg')
 
 convertForm.addEventListener('submit', function(ev) {
-    ev.preventDefault();
-})
+    ev.preventDefault()
+    console.info("I was submitted")
+    pricePounds = document.getElementById("pounds").value
+    priceEuros = pricePounds*exchangeRate
+    msg.innerHTML = "You will get: " + priceEuros + " euros"
 
-document.getElementById('pounds').value;
+    if(isNaN(pricePounds)) {
+        msg.style.display = "block"
+        msg.innerHTML = "You must enter a number"
+        msg.setAttribute('class', 'error')
+    }
+    else if(pricePounds === 0) {
+        msg.style.display = "block"
+        msg.innerHTML = "You must enter a more than 0"
+        msg.setAttribute('class', 'error')
+    }
+})
 
 //end
 })()
